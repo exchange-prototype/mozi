8fde52a171467b1749665952a2ccb3fdbac05b98
XD <1469174219@qq.com>
2018-05-04 23:38:08 +1000

    Interface part is added

---
+++ contract/smartcontract.sol
@@ -0,0 +1,10 @@
+contract token
+{
+
+function totalSupply() constant returns (uint);
+  function balanceOf(address _owner) constant returns (uint balance);
+  function transfer(address _to, uint _value) returns (bool success);
+  function transferFrom(address _from, address _to, uint _value) returns (bool success);
+  function approve(address _spender, uint _value) returns (bool success);
+  function allowance(address _owner, address _spender) constant returns (uint remaining);
+}
